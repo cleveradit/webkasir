664713ff8e1cb11979331b4e7fdbcc2ee40cecca
Muhammadabdulgani60 <muhammadabdulgani60@gmail.com>
2023-10-14 12:02:28 +0700

    fixing path css js login

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -3,21 +3,20 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 3 | Log in</title>
+  <title>Apps Coba | <?= $title ?></title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="lgn/all.min.css">
+  <link rel="stylesheet" href="<?= base_url('assets/template') ?>/plugins/fontawesome-free/css/all.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="lgn/icheck-bootstrap.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="lgn/adminlte.min.css">
+  <link rel="stylesheet" href="<?= base_url('assets/template') ?>/dist/css/adminlte.min.css">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
+
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
@@ -86,11 +85,13 @@
 <!-- /.login-box -->
 
 <!-- jQuery -->
-<script src="lgn/jquery.min.js"></script>
+<script src="<?= base_url('assets/template') ?>/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="lgn/bootstrap.bundle.min.js"></script>
+<script src="<?= base_url('assets/template') ?>/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="lgn/adminlte.min.js"></script>
+<script src="<?= base_url('assets/template') ?>/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?= base_url('assets/template') ?>/dist/js/demo.js"></script>
 
 </body>
 </html>
